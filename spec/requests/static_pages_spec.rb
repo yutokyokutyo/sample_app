8cb8696d14b537a6e7b61c12f561923f4c7acb01
近藤裕斗(kyokutyo) <k.yuto.cre@gmail.com>
2014-07-09 14:41:11 +0900

    Exercises commit (add contact fix)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -41,16 +41,16 @@ describe "Static pages" do
     end
   end
     
-    describe "Contact" do
+  describe "Contact" do
 
-      it "should have the content 'Contact'" do
-        visit '/static_pages/Contact'
-        expect(page).to have_content('Contact')
-      end
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
 
-      it "should have the title 'Contact'" do
-        visit '/static_pages/Contact'
-        expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
-      end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
   end
 end
