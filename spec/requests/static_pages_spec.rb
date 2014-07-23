dbaf7783f0ee138c2df0018b2ded27d1b1954630
近藤裕斗(kyokutyo) <k.yuto.cre@gmail.com>
2014-07-23 17:15:24 +0900

    【Exercises5_6_3】ルーティングテストを追加しました。

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,4 +32,19 @@ describe "Static pages" do
     it { should have_content('Contact') }
     it { should have_title(full_title('Contact')) }
   end
+  
+  it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      expect(page).to have_title(full_title('About Us'))
+      click_link "Help"
+      expect(page).to have_title(full_title('Help'))
+      click_link "Contact"
+      expect(page).to have_title(full_title('Contact'))
+      click_link "Home"
+      click_link "Sign up now!"
+      expect(page).to have_title(full_title('Sign up'))
+      click_link "sample app"
+      expect(page).to have_title(full_title(''))
+    end
 end
