4feae1f478f519eacfe3e9e56285f586be4977ab
近藤裕斗(kyokutyo) <k.yuto.cre@gmail.com>
2014-07-23 15:48:34 +0900

    【Exercises5_6_1】fix indent

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -27,9 +27,9 @@ describe "Static pages" do
   end
 
   describe "Contact page" do
-      before { visit contact_path }
+    before { visit contact_path }
 
-       it { should have_selector('h1', text: 'Contact') }
-      it { should have_title(full_title('Contact')) }
-    end
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
 end
