5cdfd68975291331c1136abd0bca099a2bb6d2cd
近藤裕斗(kyokutyo) <k.yuto.cre@gmail.com>
2014-08-01 16:40:34 +0900

    【Exercises6_5_2】before_saveコールバックの冗長性を解消しました。加えてインデントを修正しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
   before_save { email.downcase! }
-  #before_save { self.email = email.downcase }
+# before_save { self.email = email.downcase } 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-#has_secure_password
   validates :password, length: { minimum: 6 }
 end
