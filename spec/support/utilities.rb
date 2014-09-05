2ce7ecdf8256646fe829b48527f74c0492c92618
近藤裕斗(kyokutyo) <k.yuto.cre@gmail.com>
2014-09-05 15:55:30 +0900

    【Exercises9_6_5】Viewについての重複するコードをパーシャルを用いて排除し、リファクタリングしました。

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -10,7 +10,7 @@ def valid_signup
   fill_in "Name",         with: "Example User"
   fill_in "Email",        with: "user@example.com"
   fill_in "Password",     with: "foobar"
-  fill_in "Confirmation", with: "foobar"
+  fill_in "Confirm Password", with: "foobar"
 end
 
 RSpec::Matchers.define :have_error_message do |message|
